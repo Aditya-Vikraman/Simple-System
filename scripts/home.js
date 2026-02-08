c37f36124ce4c352ff7fcfcc0c82375df6d6fd13
Aditya <adityavikraman@gmail.com>
2026-02-08 18:37:06 +0530

    search logic and laptop updated

--- scripts/home.js
+++ scripts/home.js
@@ -18,85 +18,112 @@ searchElement.addEventListener('keydown', (event) => {
 const url = new URL (window.location.href);
 const search = url.searchParams.get('search');
 
-let filteredLaptops = laptops
+let filteredLaptops = laptops;
 
 if (search) {
-  const newArray = filteredLaptops.filter(checkLaptops)
+  const searchArray = search.split(" ");
+
+  const newArray = filteredLaptops.filter(checkLaptops);
 
   function checkLaptops (laptop) {
+    return searchArray.some((searchItem) => {
+      return Object.keys(laptop).some(key => {
+        return String(laptop[key]).toLowerCase().includes(`${searchItem.toLowerCase()}`)
+      })
+    });
+
+    /*
+    return searchArray.some((searchItem) => laptop.name.toLowerCase().includes(`${searchItem.toLowerCase()}`)  ||
+      laptop.cpu.toLowerCase().includes(`${searchItem.toLowerCase()}`) ||
+      laptop.gpu.toLowerCase().includes(`${searchItem.toLowerCase()}`) ||
+      laptop.ram.toLowerCase().includes(`${searchItem.toLowerCase()}`)||
+      laptop.storage.toLowerCase().includes(`${searchItem.toLowerCase()}`) ||
+      laptop.display.toLowerCase().includes(`${searchItem.toLowerCase()}`)
+    );
+    */
+    
+    /*
     return laptop.name.toLowerCase().includes(`${search.toLowerCase()}`)  ||
     laptop.cpu.toLowerCase().includes(`${search.toLowerCase()}`) ||
     laptop.gpu.toLowerCase().includes(`${search.toLowerCase()}`) ||
     laptop.ram.toLowerCase().includes(`${search.toLowerCase()}`)||
     laptop.storage.toLowerCase().includes(`${search.toLowerCase()}`) ||
     laptop.display.toLowerCase().includes(`${search.toLowerCase()}`)
+    */
   }
-
+  
   filteredLaptops = newArray;
 }
 
 let productListHTML = '';
 
-filteredLaptops.forEach ((product) => {
-productListHTML += `
-  <div class="product-container">
-    <div class="product-grid">
-      <div class="product-image-container js-product-image-container">
-        <img class="product-image js-product-image" src="${product.image}">
-      </div>
-      <div class="info-grid">
-        <div class="laptop-name">
-          ${product.name}
-        </div>
-        <div>
-          <p class="section">
-            CPU
-          </p>
-          <p class="cpu">
-            ${product.cpu}
-          </p>
-        </div>
-        <div>
-          <p class="section">
-            RAM
-          </p>
-          <p class="ram">
-            ${product.ram}
-          </p>
-        </div>
-        <div>
-          <p class="section">
-            GPU
-          </p>
-          <p class="gpu">
-            ${product.gpu}
-          </p>
-        </div>
-        <div>
-          <p class="section">
-            Storage
-          </p>
-          <p class="storage">
-            ${product.storage}
-          </p>
-        </div>
-        <div>
-          <p class="section">
-            Display
-          </p>
-          <p class="display">
-            ${product.display}
-          </p>
-        </div>
-        <p class="price">
-          Price: &#x20B9; ${formatCurrency(product.price)}
-        </p>
-      </div>
+if(filteredLaptops.length === 0) {
+  productListHTML = `
+    <div class="search-failed">
+    Sorry, no products matched your search for: "${search}"
     </div>
-  </div>
-
-`
-});
+  `;
+} else {
+  filteredLaptops.forEach ((product) => {
+    productListHTML += `
+      <div class="product-container">
+        <div class="product-grid">
+          <div class="product-image-container js-product-image-container">
+            <img class="product-image js-product-image" src="${product.image}">
+          </div>
+          <div class="info-grid">
+            <div class="laptop-name">
+              ${product.name}
+            </div>
+            <div>
+              <p class="section">
+                CPU
+              </p>
+              <p class="cpu">
+                ${product.cpu}
+              </p>
+            </div>
+            <div>
+              <p class="section">
+                RAM
+              </p>
+              <p class="ram">
+                ${product.ram}
+              </p>
+            </div>
+            <div>
+              <p class="section">
+                GPU
+              </p>
+              <p class="gpu">
+                ${product.gpu}
+              </p>
+            </div>
+            <div>
+              <p class="section">
+                Storage
+              </p>
+              <p class="storage">
+                ${product.storage}
+              </p>
+            </div>
+            <div>
+              <p class="section">
+                Display
+              </p>
+              <p class="display">
+                ${product.display}
+              </p>
+            </div>
+            <p class="price">
+              Price: &#x20B9; ${formatCurrency(product.price)}
+            </p>
+          </div>
+        </div>
+      </div>
+    `
+  });
+}
 
 function formatCurrency (cost) {
   let costString = cost.toLocaleString("en-IN", {style:"currency", currency: "INR", maximumFractionDigits: 0 });
